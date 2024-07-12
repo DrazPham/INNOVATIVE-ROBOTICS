6dd304a981828b1af48bd70f77c4e5e51bcf4844
DrazPham <drazpham08@gmail.com>
2024-07-12 16:38:46 +0700

    FIRESTORE DATABASE

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+//FIREBASE
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getFirestore, collection, getDocs, orderBy, deleteDoc, doc,query,limit } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBi6nOnQGYSgtU8j52O-ZhLl1fq_5tPpmM",
+
+    authDomain: "innovative-robotics.firebaseapp.com",
+
+    projectId: "innovative-robotics",
+
+    storageBucket: "innovative-robotics.appspot.com",
+
+    messagingSenderId: "713802242207",
+
+    appId: "1:713802242207:web:d14a1e3c5f5b54b5aceaaa"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const event = document.getElementById("event");
 
-const fetchAPI = async() => {
-    const responsive =  await fetch(`event.json`)
-    let data = await responsive.json();
-    var jsonContainer = data;
-    for (i in jsonContainer){
-        let post_link = document.createElement("a");
-        post_link.href = `${jsonContainer[i].link}`;
-        post_link.innerHTML = `
-            <div class = "event-container">
-                <img class = "event-image" src="${jsonContainer[i].img_url}" alt="">
-                <div>
-                    <h3>${jsonContainer[i].title}</h3>
-                    <p>${jsonContainer[i].detail}</p>
-                </div>
-            </div>`
+var count = 0;
+// const querySnapshot = await getDocs(collection(db, "news"));
+const querySnapshot = await getDocs(
+    query(
+        collection(db, "news"), 
+        orderBy("date", "desc"), 
+        limit(3)
+    )
+);
+querySnapshot.forEach((doc) => {
+    var jsonContainer = doc.data();
+    let post_link = document.createElement("a");
+    post_link.href = `${jsonContainer.link}`;
+    post_link.innerHTML = `
+        <div class = "event-container">
+            <img class = "event-image" src="${jsonContainer.image_url}" alt="">
+            <div>
+                <h3>${jsonContainer.title}</h3>
+                <p>${jsonContainer.detail}</p>
+            </div>
+        </div>`
     event.appendChild(post_link);
-    }
-}
-fetchAPI();
+});
